0ce5a8b46ced3dcafee359c7212d187b89d5f652
gregnani <fgregnani@gmail.com>
2023-04-02 19:00:22 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("AsciiArt");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine(" ||  || ");
+Console.WriteLine(@" \\()// ");
+Console.WriteLine(@"//(__)\\");
+Console.WriteLine("||    ||");
+
+Console.ResetColor();
